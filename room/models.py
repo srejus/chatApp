b85517526ed4a6756a782b92a53739a9469d063f
srejus <srejusjayaakumar@gmail.com>
2022-07-31 13:57:47 +0530

    Image send option added

--- room/models.py
+++ room/models.py
@@ -9,3 +9,4 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+    
--- room/views.py
+++ room/views.py
@@ -10,11 +10,15 @@ def rooms(request):
     user_agent = request.META['HTTP_USER_AGENT']
 
     if 'Mobile' in user_agent:
-        return render(request,'test.html')
-    return render(request,'room.html',{'rms':rooms,'flag':flag})
+        return render(request,'mobile/rooms.html',{'rms':rooms})
+    return render(request,'rooms.html',{'rms':rooms,'flag':flag})
 
 def room(request,slug):
     room = Room.objects.get(slug=slug)
     rooms = Room.objects.all()
     flag = 1
+    user_agent = request.META['HTTP_USER_AGENT']
+
+    if 'Mobile' in user_agent:
+        return render(request,'mobile/room.html',{'room':room})
     return render(request,'room.html',{'rms':rooms,'flag':flag,'room':room})

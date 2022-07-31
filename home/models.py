87776ee5e42786091178253f5ac1826ff9fb2750
srejus <srejusjayaakumar@gmail.com>
2022-08-01 02:23:03 +0530

    User Account models added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user')
+    name = models.CharField(max_length=100,null=True,blank=True)
+    profile_picture = models.ImageField(null=True,blank=True,upload_to='user_profile')
+    desc = models.CharField(max_length=150,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
--- room/models.py
+++ room/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=50)
     profile_picture = models.ImageField(null=True,blank=True)
